1677b8c0160e0fb73328f2d04edcce0b5ff4a1cd
rantaroyuri <ksyuhan4eg@gmail.com>
2021-09-22 21:00:06 +0300

    добавила main, работа с битовыми операциями

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
+//minGW(Windows) (GCC) Linux Clang - три самых известных компилятора
 #include <iostream>
-#include <stdio.h>
-using namespace std;
 
-int main(){
-    cout<<"РёСЃРїСЂР°РІР»РµРЅРёРµ!";
-return 0;
+//using namespace std;
+//using std::cout;
+namespace s{
+    int x = 3;
+}
+
+
+
+
+
+int main()
+{
+    int y = s::x<<1;
+
+    std::cout << s::x << std::endl;
+    std::cout << y << std::endl;
+    std::cout << (y<<1) << std::endl;
+    std::cout << (y<<2) << std::endl;
+    return 0;
 }
